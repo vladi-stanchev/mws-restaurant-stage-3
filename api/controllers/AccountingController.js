bd6250c64743bd2e7469ddbe7cd5575a3ed10f05
Vladi Stanchev <35735302+vladi-stanchev@users.noreply.github.com>
2018-09-18 21:22:57 +0100

    Add Accounting Controller

---
+++ api/controllers/AccountingController.js
@@ -0,0 +1,8 @@
+/**
+ * AccountingController
+ *
+ * @description :: Server-side logic for managing restaurants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
